690542aa45d0aea4934cfcc1599911c7e680c81d
Caroline Crooks <carolinecrooks@Caroline-Crooks.local>
2020-09-13 14:25:25 -0400

    finetune website

--- app.js
+++ app.js
@@ -1,61 +1,79 @@
-
-
+//get submit button, input bar, and php code
 var submit = document.getElementById("submitButton");
-var phpLink = "twitterRequest.php";
 var inputBar = document.getElementById("linkInput");
-var downloadIcon = document.getElementById("downloadIcon");
+var phpLink = "twitterRequest.php";
 var tweetURL;
 var tweetID;
 
-submit.addEventListener("click", function(e){
-  console.log('submit');
+//when submit is clicked
+submit.addEventListener("click", function (e) {
+  console.log("submit");
   e.preventDefault();
   tweetURL = inputBar.value;
-  inputBar.value = "";
-  if (tweetURL.includes("twitter") && tweetURL.includes("status")){
+  inputBar.value = ""; //clear input bar text
+  if (tweetURL.includes("twitter") && tweetURL.includes("status")) {
+    //check if a valid twitter link
     tweetID = getID(tweetURL);
-    console.log('tweetID: ' + tweetID);
+    console.log("tweetID: " + tweetID);
     var hr = new XMLHttpRequest();
-    hr.onreadystatechange = function(){
-      if (this.readyState === 4 && this.status === 200){
+    hr.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
         inputBar.value = "";
-        var response = JSON.parse(this.response);
-        if (response.valid){
+        var response = JSON.parse(this.response); //handle php response
+        console.log(response);
+        if (response.valid) {
           var formattedURL = formatURL(response.message);
-          window.open(formattedURL, '_blank');
-          inputBar.setAttribute("placeholder", "");
-          downloadIcon.style.display = "block";
-          downloadIcon.setAttribute("href", formattedURL);
+          console.log(formattedURL);
+          downloadMedia(formattedURL, tweetID);
+          inputBar.setAttribute("placeholder", ""); //clear text
         } else {
           errorMessage(response.message);
         }
-      };
+      }
     };
+
+    //handle request with php
     hr.open("POST", phpLink, true);
     hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    hr.responseType="text";
+    hr.responseType = "text";
     hr.send("linkInput=" + tweetID);
   } else {
     errorMessage("input a video link");
   }
 });
 
-function errorMessage(message){
+function errorMessage(message) {
+  //error if not a twitter video
   console.log(message);
   inputBar.setAttribute("placeholder", "input a twitter video link");
-  downloadIcon.style.display = 'none';
-  downloadIcon.setAttribute("href", "");
-};
+}
 
-function getID(url){
+function getID(url) {
+  //get tweet ID from the link
   var firstIndex = url.indexOf("status") + 7;
   var lastIndex = firstIndex;
-  while (!isNaN(url[lastIndex])){
+  while (!isNaN(url[lastIndex])) {
     lastIndex++;
   }
   return url.slice(firstIndex, lastIndex);
 }
 
-function formatURL(url){
-  return url.replace("\/", "/");
+function formatURL(url) {
+  return url.replace("/", "/");
+}
+
+async function downloadMedia(twitterURL, fileName) {
+  //create Blob from the video link and download to computer
+  const response = await fetch(twitterURL, {
+    method: "GET",
+    mode: "cors",
+  });
+  const myBlob = await response.blob();
+  const url = window.URL.createObjectURL(new Blob([myBlob]));
+  const a = document.createElement("a");
+  a.href = url;
+  a.setAttribute("download", fileName + ".mp4");
+  document.body.appendChild(a);
+  a.click();
+  a.parentNode.removeChild(a);
 }
--- twitterRequest.php
+++ twitterRequest.php
@@ -1,27 +1,28 @@
 <?php
   $tweetID = $_POST['linkInput'];
-  require("twitteroauth/autoload.php");
+
+  //connect to twitter
+  require("vendor/autoload.php");
   use Abraham\TwitterOAuth\TwitterOAuth;
-
-  define ('CONSUMER_KEY', '');
-  define('CONSUMER_SECRET', '');
-  $access_token = '';
-  $access_token_secret = '';
-
+  define ('CONSUMER_KEY', 'KokOVScxygQ5IQNsTxQU2qw4Z');
+  define('CONSUMER_SECRET', 'T8ONXHLNWtndPNZRl4alDGwE5AtL32mD2L9Pfm3WJR4djmZvvy');
+  $access_token = '1195590715193135105-TMvE3xL0VoGZniJoVX8uefZdjBPeXU';
+  $access_token_secret = 'oG32XzUx2mYTvIJTlpNHZoGtpNWbEJFBX4KCzeNly0PMf';
   $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_token_secret);
   $content = $connection->get("account/verify_credentials");
   $statuses = $connection->get("statuses/show", ["id" => $tweetID]);
+
+  //get json array and parse through it
   $jsonArray = json_decode(json_encode($statuses), true);
   $videoURL = "";
   $errorMessage = "";
   $videoValid = true;
-
   try {
     if (isset($jsonArray["errors"])){
-      throw new Exception($jsonArray["errors"]["0"]["message"]);
+      throw new Exception($jsonArray["errors"]["0"]["message"]); //stop if twitter oauth error 
     } elseif (!isset($jsonArray["extended_entities"]["media"][0]["video_info"]["variants"])){
-      throw new Exception("not a video");
-    } else {
+      throw new Exception("not a video"); //stop if not a video or gif
+    } else { //get the video link
       $videoVariants = $jsonArray["extended_entities"]["media"][0]["video_info"]["variants"];
       $videoVariantsCount = count($videoVariants);
       $mp4Present = false;
@@ -43,7 +44,7 @@
     $videoValid = false;
   }
 
-  if ($videoValid){
+  if ($videoValid){ //response back to app.js, both if it works or did not work 
     echo json_encode(array(
       "valid" => true,
       "message" => $videoURL
